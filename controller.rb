eac61cc7531e67e764209f8b03a67db6c98f1d1b
David Hale <dhale12@hotmail.co.uk>
2017-02-22 21:22:24 +0000

    add show to view 1 player

--- controller.rb
+++ controller.rb
@@ -11,6 +11,11 @@ get '/players/new' do
   erb(:new)
 end
 
+get '/players/:id' do
+  @player = Player.find(params[:id])
+  erb(:show)
+end
+
 post '/players' do
   player = Player.new(params)
   player.save
