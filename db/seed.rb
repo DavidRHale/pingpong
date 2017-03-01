d870f2a20d370246d6ac5438efa8bd1c2da09759
David Hale <dhale12@hotmail.co.uk>
2017-03-01 17:09:16 +0000

    change seed

--- db/seed.rb
+++ db/seed.rb
@@ -12,44 +12,53 @@ TournamentPlayer.delete_all
 PlayerGame.delete_all
 
 player1 = Player.new({
-  'name' => 'David Hale',
-  'nickname' => 'The Admiral',
+  'name' => 'Bruce Banner',
+  'nickname' => 'The Incredible Hulk',
   'dominant_hand' => 'Right',
-  'skill_set' => 'Backhand God',
+  'skill_set' => 'Smash',
   'win_count' => 0,
   'loss_count' => 0
   })
 
 player2 = Player.new({
-  'name' => 'Lorna Hale',
-  'nickname' => 'The Wimp',
+  'name' => 'Tony Stark',
+  'nickname' => 'Iron Man',
   'dominant_hand' => 'Right',
-  'skill_set' => 'None',
+  'skill_set' => 'Flying Forehand',
   'win_count' => 0,
   'loss_count' => 0
   })
 
 player3 = Player.new({
-  'name' => 'Winston Hale',
-  'nickname' => 'The Cat',
+  'name' => 'Thor Odinson',
+  'nickname' => 'Thor',
   'dominant_hand' => 'Right',
-  'skill_set' => 'None',
+  'skill_set' => 'Lightning shots',
   'win_count' => 0,
   'loss_count' => 0
   })
 
 player4 = Player.new({
-  'name' => 'Jimmy',
-  'nickname' => 'Jim',
+  'name' => 'Steve Rogers',
+  'nickname' => 'Captain America',
   'dominant_hand' => 'Right',
-  'skill_set' => 'None',
+  'skill_set' => 'Block',
   'win_count' => 0,
   'loss_count' => 0
   })
 
 player5 = Player.new({
-  'name' => 'Harold',
-  'nickname' => 'Harry',
+  'name' => 'Scott Lang',
+  'nickname' => 'Ant-Man',
+  'dominant_hand' => 'Right',
+  'skill_set' => 'Drop shot',
+  'win_count' => 0,
+  'loss_count' => 0
+  })
+
+player6 = Player.new({
+  'name' => 'Peter Parker',
+  'nickname' => 'Spider-Man',
   'dominant_hand' => 'Right',
   'skill_set' => 'None',
   'win_count' => 0,
@@ -61,6 +70,7 @@ player2.save
 player3.save
 player4.save
 player5.save
+player6.save
 
 tournament1 = Tournament.new({'name' => 'Master League', 'format' => 'League'})
 tournament2 = Tournament.new({'name' => 'Master Cup', 'format' => 'Knock-Out'})
@@ -70,15 +80,23 @@ tournament1.save
 tournament2.save
 tournament3.save
 
+tournament1.add_player(player1.id)
+tournament1.add_player(player2.id)
+tournament1.add_player(player3.id)
+tournament1.add_player(player4.id)
+tournament1.add_player(player5.id)
+tournament1.add_player(player6.id)
+
+tournament1.create_league_fixtures
+
 tournament2.add_player(player1.id)
 tournament2.add_player(player2.id)
 tournament2.add_player(player3.id)
 tournament2.add_player(player4.id)
 tournament2.add_player(player5.id)
+tournament2.add_player(player6.id)
 
 tournament2.create_knockout_round
-
-
 
 binding.pry
 nil
